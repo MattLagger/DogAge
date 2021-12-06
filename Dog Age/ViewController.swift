d2fc27918fff0f70ea5b2085cd8d002a05276504
Wendreo Matheus <wendreo.pvh@gmail.com>
2021-12-05 21:29:32 -0300

    Initial Commit

--- Dog Age/ViewController.swift
+++ Dog Age/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var dogAgeField: UITextField!
+    
+    @IBAction func discoverAge(_ sender: Any) {
+        let idade = Int(dogAgeField.text!)! * 7
+        resultLabel.text = String("The dog age is: \(idade)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
